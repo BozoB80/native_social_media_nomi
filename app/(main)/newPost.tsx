83c103030a178237af80ac3e4c4c4121299b3892
Bozo Bakula <bbakula@royanis.com>
2024-12-04 13:56:14 +0100

    post

--- app/(main)/newPost.tsx
+++ app/(main)/newPost.tsx
@@ -1,4 +1,10 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React, { useRef, useState } from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Header from "@/components/Header";
@@ -8,6 +14,11 @@ import Avatar from "@/components/Avatar";
 import { useAuth } from "@/contexts/AuthContext";
 import TextEditor from "@/components/TextEditor";
 import { useRouter } from "expo-router";
+import Icon from "@/assets/icons";
+import Button from "@/components/Button";
+import * as ImagePicker from "expo-image-picker";
+import { Image } from "expo-image";
+import { getSupabaseFileUrl } from "@/services/imageService";
 
 const NewPost = () => {
   const { user } = useAuth();
@@ -15,7 +26,74 @@ const NewPost = () => {
   const editorRef = useRef(null);
   const router = useRouter();
   const [loading, setLoading] = useState(false);
-  const [file, setFile] = useState("");
+  const [file, setFile] = useState<ImagePicker.ImagePickerAsset | null>(null);
+
+  const onPick = async (isImage: boolean) => {
+    let mediaConfig: ImagePicker.ImagePickerOptions = {
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      aspect: [4, 3] as [number, number],
+      quality: 0.7,
+    };
+
+    if (!isImage) {
+      mediaConfig = {
+        mediaTypes: ["videos"],
+        allowsEditing: true,
+        aspect: [4, 3] as [number, number],
+        quality: 0.7,
+      };
+    }
+
+    let result = await ImagePicker.launchImageLibraryAsync(mediaConfig);
+
+    if (!result.canceled) {
+      setFile(result.assets[0]);
+    }
+  };
+
+  const isLocalFile = (file: ImagePicker.ImagePickerAsset) => {
+    if (!file) {
+      return null;
+    }
+
+    if (typeof file === "object") {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const getFileType = (file: ImagePicker.ImagePickerAsset) => {
+    if (!file) {
+      return null;
+    }
+
+    if (isLocalFile(file)) {
+      return file.type;
+    }
+
+    // check image or video for remote file
+    if (file.type?.includes("postImage")) {
+      return "image";
+    }
+
+    return "video";
+  };
+
+  const getFileUri = (file: ImagePicker.ImagePickerAsset) => {
+    if (!file) {
+      return null;
+    }
+
+    if (isLocalFile(file)) {
+      return file.uri;
+    }
+
+    return getSupabaseFileUrl(file as unknown as string)?.uri;
+  };
+
+  const onSubmit = async () => {};
 
   return (
     <ScreenWrapper bg="white">
@@ -30,13 +108,46 @@ const NewPost = () => {
             />
             <View style={{ gap: 2 }}>
               <Text style={styles.username}>{user && user?.name}</Text>
+              <Text style={styles.publicText}>Public</Text>
             </View>
           </View>
 
           <View style={styles.textEditor}>
-            <TextEditor />
+            <TextEditor
+              editorRef={editorRef}
+              onChange={(body) => (bodyRef.current = body)}
+            />
+          </View>
+
+          {file && (
+            <View style={styles.file}>
+              {getFileType(file) == "video" ? (
+                <></>
+              ) : (
+                <Image source={{ uri: getFileUri(file) }} style={{ flex: 1 }} />
+              )}
+            </View>
+          )}
+
+          <View style={styles.media}>
+            <Text style={styles.addImageText}>Add to your post</Text>
+            <View style={styles.mediaIcons}>
+              <TouchableOpacity onPress={() => onPick(true)}>
+                <Icon name="image" size={30} color={theme.colors.dark} />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => onPick(false)}>
+                <Icon name="video" size={33} color={theme.colors.dark} />
+              </TouchableOpacity>
+            </View>
           </View>
         </ScrollView>
+        <Button
+          title="Post"
+          loading={loading}
+          hasShadow={false}
+          onPress={onSubmit}
+          buttonStyle={{ height: hp(6.2) }}
+        />
       </View>
     </ScreenWrapper>
   );
@@ -100,5 +211,26 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     gap: 15,
+  },
+  addImageText: {
+    fontSize: hp(1.9),
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text,
+  },
+  imageIcon: {
+    borderRadius: theme.radius.md,
+  },
+  file: {
+    height: hp(30),
+    width: "100%",
+    borderRadius: theme.radius.md,
+    overflow: "hidden",
+    borderCurve: "continuous",
+  },
+  video: {},
+  closeIcon: {
+    position: "absolute",
+    top: 10,
+    right: 10,
   },
 });
--- components/TextEditor.tsx
+++ components/TextEditor.tsx
@@ -1,12 +1,85 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { theme } from "@/constants/theme";
+import { View, Text, StyleSheet } from "react-native";
+import {
+  actions,
+  RichEditor,
+  RichToolbar,
+} from "react-native-pell-rich-editor";
 
-const TextEditor = () => {
+type TexextEditorProps = {
+  editorRef: any;
+  onChange: (body: string) => void;
+};
+
+const TextEditor = ({ editorRef, onChange }: TexextEditorProps) => {
   return (
-    <View>
-      <Text>TextEditor</Text>
+    <View style={{ minHeight: 285 }}>
+      <RichToolbar
+        editor={editorRef}
+        disabled={false}
+        iconMap={{
+          [actions.heading1]: ({ tintColor }: any) => (
+            <Text style={{ color: tintColor }}>H1</Text>
+          ),
+          [actions.heading4]: ({ tintColor }: any) => (
+            <Text style={{ color: tintColor }}>H4</Text>
+          ),
+        }}
+        actions={[
+          actions.heading1,
+          actions.heading4,
+          actions.setBold,
+          actions.setItalic,
+          actions.setUnderline,
+          actions.setStrikethrough,
+          actions.insertBulletsList,
+          actions.blockquote,
+          actions.alignLeft,
+          actions.alignCenter,
+          actions.alignRight,
+          actions.code,
+          actions.line,
+          actions.undo,
+          actions.redo,
+        ]}
+        selectedIconTint={theme.colors.primaryDark}
+        style={styles.richBar}
+        flatContainerStyle={styles.flatStyle}
+      />
+      <RichEditor
+        ref={editorRef}
+        containerStyle={styles.rich}
+        editorStyle={styles.contentStyle}
+        placeholder="What's on your mind"
+        onChange={onChange}
+      />
     </View>
   );
 };
 
 export default TextEditor;
+
+const styles = StyleSheet.create({
+  richBar: {
+    borderTopRightRadius: theme.radius.xl,
+    borderTopLeftRadius: theme.radius.xl,
+    backgroundColor: theme.colors.gray,
+  },
+  flatStyle: {
+    paddingHorizontal: 8,
+    gap: 3,
+  },
+  rich: {
+    minHeight: 240,
+    flex: 1,
+    borderWidth: 1.5,
+    borderTopWidth: 0,
+    borderBottomRightRadius: theme.radius.xl,
+    borderBottomLeftRadius: theme.radius.xl,
+    borderColor: theme.colors.gray,
+    padding: 5,
+  },
+  contentStyle: {
+    color: theme.colors.textDark,
+  },
+});
